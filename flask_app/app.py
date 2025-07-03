8917e3a2506ab4e86aacb4b65dc1db934c6a2244
Akanksha Tanwar <tanwarakanksha1996@gmail.com>
2025-07-03 23:07:49 +0200

    deploy

--- flask_app/app.py
+++ flask_app/app.py
@@ -69,5 +69,5 @@ def predict():
         return {"error": "No prediction found for the given year and month."}
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    port = int(os.environ.get('PORT', 8080))  # Use PORT env variable
+    app.run(host='0.0.0.0', port=port)
